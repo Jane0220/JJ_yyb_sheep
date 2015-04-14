325ceb6d48cf118f18c7a2077f173e3ef24ed898
jalc <jalc@yunio.com>
2015-04-14 17:23:14 +0800

    upload_ajax

--- upload_ajax.php
+++ upload_ajax.php
@@ -1,6 +1,5 @@
 <?php
 //include_once('session.php');
-/*
 if( isset($_POST['fold_name']) && $_POST['fold_name'] ){
 	$fold_name = $_POST['fold_name'];
 }else{
@@ -9,7 +8,19 @@ if( isset($_POST['fold_name']) && $_POST['fold_name'] ){
 	echo json_encode($data);
 	exit;
 }
-*/
-$_POST['folder_name']
-$_FILES['file']
+$file = $_FILES['file'];
+$file_name = $_FILES['file']['name'];
+$file_folder = "images/".$fold_name;
+if(!file_exists($file_folder)){
+	$result = mkdir($file_folder,0777);
+}
+$path = 'images/'.$fold_name."/".$file_name;
+$result = move_uploaded_file($file,$path);
+if($result){
+	$data['code'] = 1;
+	$data['data'] = $fold_name."/".$file_name;
+}else{
+	$data['code'] = 0;
+	$data['data'] = '';
+}
 ?>

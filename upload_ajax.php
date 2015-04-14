1caeef1de250f5ed28da6d8f1a83aeb54cca2f9a
jalc <jalc@yunio.com>
2015-04-14 17:28:56 +0800

    upload_ajax

--- upload_ajax.php
+++ upload_ajax.php
@@ -1,24 +1,24 @@
 <?php
 //include_once('session.php');
-if( isset($_POST['fold_name']) && $_POST['fold_name'] ){
-	$fold_name = $_POST['fold_name'];
+if( isset($_POST['folder_name']) && $_POST['folder_name'] ){
+	$folder_name = $_POST['folder_name'];
 }else{
 	$data['code'] = 0;
-	$data['data'] = "fold_name is null, bad_request";
+	$data['data'] = "folder_name is null, bad_request";
 	echo json_encode($data);
 	exit;
 }
 $file = $_FILES['file'];
 $file_name = $_FILES['file']['name'];
-$file_folder = "images/".$fold_name;
+$file_folder = "images/".$folder_name;
 if(!file_exists($file_folder)){
 	$result = mkdir($file_folder,0777);
 }
-$path = 'images/'.$fold_name."/".$file_name;
+$path = 'images/'.$folder_name."/".$file_name;
 $result = move_uploaded_file($file,$path);
 if($result){
 	$data['code'] = 1;
-	$data['data'] = $fold_name."/".$file_name;
+	$data['data'] = $folder_name."/".$file_name;
 }else{
 	$data['code'] = 0;
 	$data['data'] = '';

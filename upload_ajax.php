0261dfc096c074752d0b09c1b1dc9dc7f6128683
jalc <jalc@yunio.com>
2015-04-14 16:10:35 +0800

    upload_ajax

---
+++ upload_ajax.php
@@ -0,0 +1,23 @@
+<?php
+//include_once('session.php');
+/*
+if( isset($_POST['fold_name']) && $_POST['fold_name'] ){
+	$fold_name = $_POST['fold_name'];
+}else{
+	$data['code'] = 0;
+	$data['data'] = "fold_name is null, bad_request";
+	echo json_encode($data);
+	exit;
+}
+if( isset($_POST['files']) && $_POST['files'] ){
+	$files = $_POST['files'];
+}else{
+	$data['code'] = 0;
+	$data['data'] = "files is null, bad_request";
+	echo json_encode($data);
+	exit;
+}
+*/
+var_dump($_POST['fold_name']);
+var_dump($_POST['files']);
+?>

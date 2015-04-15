1aa75ec48e9172df112b2a99ffe135f96e45af61
jalc <jalc@yunio.com>
2015-04-15 10:22:25 +0800

    upload

--- upload.php
+++ upload.php
@@ -3,8 +3,6 @@ include_once('session.php');
 session_start();
 $session_array = $_SESSION;
 session_write_close();
-var_dump($session_array);
-exit;
 if(isset($session_array['yyb_backend_admin'])){
 	include_once('upload_view.php');
 	// echo 1111111;

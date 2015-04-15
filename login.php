54e2c7451c2ab67d19cc16190a7bd50726bebbb3
jalc <jalc@yunio.com>
2015-04-15 10:27:29 +0800

    upload and login

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 $session_array = $_SESSION;
 session_write_close();
 if(isset($session_array['yyb_backend_admin'])){
-	include_once('upload_view.php');
+	header("location: upload.php");
 }else{
 	include_once('login_view.php');
 }
